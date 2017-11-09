87211bc5b3ecf2ad8f7ce1e4be160fed87b9c211
andy2carrie <85748431@qq.com>
2017-11-09 16:07:15 +0800

    test git 1

---
+++ src/main/java/com/example/domain/Person.java
@@ -0,0 +1,43 @@
+package com.example.domain;
+
+/**
+ * Created by user on 2017/11/7.
+ */
+public class Person {
+    private Long id;
+    private String name;
+    private String age;
+    private String sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
